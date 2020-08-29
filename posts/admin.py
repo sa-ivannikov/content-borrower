83f78bfaa939efa0a19bc326e408d9736d2ff992
Sergey Ivannikov <s.ivannikov22@gmail.com>
2020-08-30 01:30:38 +0300

    Added new fields to admin

--- posts/admin.py
+++ posts/admin.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from .models import Post, Donor, Recipient
 
 class PostAdmin(admin.ModelAdmin):
-    fields = ['posted_at', 'likes_count', 'reposts_count', 'comments_count', 'img_links', 'donors_group']
+    fields = ['posted_at', 'likes_count', 'reposts_count', 'comments_count', 'img_links', 'for_recipient', 'from_donor', 'subs_amount']
 
 class DonorAdmin(admin.ModelAdmin):
-    fields = ['group_name', 'group_id', 'group_domain', 'vk_link']
+    fields = ['group_name', 'group_id', 'group_domain', 'vk_link', 'subs_amount']
     
 class RecipientAdmin(admin.ModelAdmin):
     fields = ['name', 'target_group_name', 'target_group_link', 'donors']
