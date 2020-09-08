c8b3182dd4febb5e018b409802c5285837976f8f
Sergey Ivannikov <s.ivannikov22@gmail.com>
2020-09-08 08:23:55 +0300

    Hid some sensetive info

--- borrower/settings.py
+++ borrower/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cdd_zbc5h#w6t$!a-&iyuh&64e5o=c@)#lw+9su&g&)%g7r9q-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
