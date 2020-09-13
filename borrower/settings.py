3e2e162397a96137455dbd65a11fabd0a86eaf05
Sergey Ivannikov <s.ivannikov22@gmail.com>
2020-09-13 18:10:01 +0300

    Some deployment preps

--- borrower/settings.py
+++ borrower/settings.py
@@ -23,8 +23,14 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
+# Deployment security
+""" CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True """
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
